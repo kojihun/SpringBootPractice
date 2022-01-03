113f524ec0345b321f319903576fae0d440b11de
kojihun <38236367+kojihun@users.noreply.github.com>
2022-01-03 23:32:24 +0900

    Object Mapper
    Text JSON -> Object
    Object -> Text JSON
    
    controller req json(text) -> object
    response object -> json(text)
    
    object -> text
    object mapper가 get method를 활용한다.
    
    text -> object
    object mapper는 default생성자를 필요로 한다.

--- src/main/java/com/example/demo/dto/UserRequest.java
+++ src/main/java/com/example/demo/dto/UserRequest.java
@@ -1,12 +1,16 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequest {
     private String name;
     private String email;
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import com.example.demo.dto.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +11,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() throws JsonProcessingException {
+		System.out.println("Hello~");
 
+		// Text JSON -> Object
+		// Object -> Text JSON
+
+		// controller req json(text) -> object
+		// response object -> json(text)
+
+		// object -> text
+		// object mapper가 get method를 활용한다.
+		var objectMapper = new ObjectMapper();
+		var user = new UserRequest("steve", "google.com");
+		var text = objectMapper.writeValueAsString(user);
+		System.out.println(text);
+
+		// text -> object
+		// object mapper는 default생성자를 필요로 한다.
+		var objectUser = objectMapper.readValue(text, UserRequest.class);
+		System.out.println(objectUser);
+	}
 }
